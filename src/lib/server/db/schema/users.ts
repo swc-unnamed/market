aac21890403b31f83c61173ae7b5b6836d25f098
swc-marc <marcinius@protonmail.com>
2025-01-18 02:44:38 -0500

    linter

--- src/lib/server/db/schema/users.ts
+++ src/lib/server/db/schema/users.ts
@@ -3,7 +3,10 @@ import { boolean, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 import { Roles } from '../../../consts/roles';
 
 export const users = pgTable('users', {
-	id: text('id').unique().primaryKey().$defaultFn(() => createId()),
+	id: text('id')
+		.unique()
+		.primaryKey()
+		.$defaultFn(() => createId()),
 	name: text('name').unique().notNull(),
 	combineId: text('combine_id').unique().notNull(),
 	discordId: text('discord_id').unique(),
@@ -12,5 +15,5 @@ export const users = pgTable('users', {
 	scopes: text('scopes'),
 	role: text('role', { enum: Roles }).default('Patron').notNull(),
 	banned: boolean('banned').default(false),
-	banned_reason: text('banned_reason'),
+	banned_reason: text('banned_reason')
 });
