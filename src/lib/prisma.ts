d807826ce6dd575eeae7238ec224635100516c3e
swc-marc <marcinius@protonmail.com>
2025-02-15 06:14:43 -0500

    extending prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,4 +2,17 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+prisma.$extends({
+	name: 'onAuctionListingCreate',
+	query: {
+		auctionListing: {
+			async create({ args, query }) {
+				const result = await query(args);
+				// TODO: Trigger redis pub/sub event
+				return result;
+			}
+		}
+	}
+});
+
 export { prisma };
