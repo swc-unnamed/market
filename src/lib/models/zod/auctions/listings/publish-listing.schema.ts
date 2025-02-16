d9ac774ce2acf066335e8657edfabf3adb089314
swc-marc <marcinius@protonmail.com>
2025-02-16 03:15:57 -0500

    fixed incorrect validation check

--- src/lib/models/zod/auctions/listings/publish-listing.schema.ts
+++ src/lib/models/zod/auctions/listings/publish-listing.schema.ts
@@ -7,7 +7,7 @@ export const publishListingSchema = z.object({
 		.max(50, { message: 'Title must be between 4 and 50 characters' }),
 	description: z.string().optional().nullable(),
 	location: z.string().max(400, { message: 'Location must be less than 400 characters' }),
-	startingPrice: z.number().int().min(1, { message: 'Starting price must be at least 1 credit' }),
+	startingBid: z.number().int().min(1, { message: 'Starting price must be at least 1 credit' }),
 	sendCreditsTo: z.string(),
-	listerIsAnon: z.boolean().optional().nullable()
+	anonymousListing: z.boolean().optional().nullable()
 });
